81072ba59905b95a79fe23dbb55cd346b27cdffb
JMurphy1 <murphy.johne@gmail.com>
2024-05-26 12:24:48 -0400

    new branch

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE observations (
+    ID SERIAL PRIMARY KEY,
+    SiteID INT,
+    ObservationTimeUTC TIMESTAMP,
+    Value FLOAT
+);
+
+-- Insert some sample data
+INSERT INTO observations (SiteID, ObservationTimeUTC, Value) VALUES
+(1, '2024-05-01 12:00:00', 25.4),
+(2, '2024-05-01 12:05:00', 26.1),
+(1, '2024-05-01 12:10:00', 25.8),
+(2, '2024-05-01 12:15:00', 26.3);
